470ab94fb50ae60b62869790b37b13d5e06a350d
workpreeti6 <94400142+workpreeti6@users.noreply.github.com>
2024-12-23 11:00:05 +0530

    feed api's

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,27 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  experimental: { // Enable experimental features
-    staleTimes: { // Configure the stale time for the cache
+  experimental: {
+    staleTimes: {
       dynamic: 30, // Set the stale time for dynamic pages to 30 seconds
     },
   },
-  // Describe in the lucia documentation
   serverExternalPackages: ["@node-rs/argon2"], // Include native package
-  images: { // Configure the image loader
-    domains: ['fileinfo.com'], // Add the domain you need to whitelist
-    // remotePatterns: [ // Match any image URL
-    //   {
-    //     protocol: "https", // Match any protocol
-    //     hostname: "utfs.io", // Match the hostname
-    //     pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`, // Match the pathname
-    //   },
-    // ],
+  images: {
+    domains: ["fileinfo.com"], // Add the domain you need to whitelist
   },
-  rewrites: async () => { // Define the rewrites
-    return [ // Return an array of rewrites
+  rewrites: async () => {
+    return [
       {
         source: "/hashtag/:tag", // Match the hashtag route
         destination: "/search?q=%23:tag", // Redirect to the search route with the hashtag query
       },
+      {
+        source: "/api/:path*", // Proxy to backend
+        destination: "http://localhost:5000/api/:path*",
+      },
     ];
-  }
+  },
 };
 
 export default nextConfig;
-
